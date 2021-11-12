e1c2061b4e3ee0d059c72806b4194d1445d78ea3
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2021-11-12 10:38:13 +0530

    Updated premake

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "off"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin/intermediates/" .. outputdir .. "/%{prj.name}")
@@ -11,12 +11,17 @@ project "GLFW"
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
 		"src/glfw_config.h",
+		"src/platform.c",
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
 		"src/vulkan.c",
 		"src/window.c"
+		"src/null_window.c",
+		"src/null_monitor.c",
+		"src/null_joystick.c",
+		"src/null_init.c"
 	}
 	filter "system:linux"
 		pic "On"
@@ -53,6 +58,7 @@ project "GLFW"
 			"src/win32_time.c",
 			"src/win32_thread.c",
 			"src/win32_window.c",
+			"src/win32_module.c",
 			"src/wgl_context.c",
 			"src/egl_context.c",
 			"src/osmesa_context.c"
@@ -67,7 +73,9 @@ project "GLFW"
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
+		buildoptions "/MTd"
 
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
+		buildoptions "/MT"
